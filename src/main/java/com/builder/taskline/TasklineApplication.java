8857376ef7e11ff95b0f1d6d1a6ee27196d67cca
Josiah DuBose <josiah7686@gmail.com>
2025-10-18 21:43:58 -0400

    Create project

---
+++ src/main/java/com/builder/taskline/TasklineApplication.java
@@ -0,0 +1,13 @@
+package com.builder.taskline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/builder/taskline/TasklineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.builder.taskline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
